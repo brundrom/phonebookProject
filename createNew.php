e4e6a55be92227896e20a84a6080c733b0779d20
pga <brundrom@gmail.com>
2022-05-10 12:52:34 +0300

    Added main menu and contact list

--- createNew.php
+++ createNew.php
@@ -1,6 +1,37 @@
+<html>
+<head>
+    <title>Account creation</title>
+</head>
+<body>
+    <!--MENU-->
+    <table>
+            <tr>
+                <td>The Phonebook Project | </td>
+                <td><a href="createNew.php">New | </a></td>
+                <td><a href="editList.php">Edit | </a></td>
+                <td><a href="search.php">Search | </a></td>
+            </tr>
+        </table>  
+    <center>
 <form method="GET" action="createAccount.php">
-    <b>Name: </b><input name="accountName" type="text"><br>
-    <b>Phone: </b><input name="accountPhone" type="text"><br>
-    <b>Address: </b><input name="accountAddress" type="text"><br>
-    <input type="submit" value="Create"><br>
+    <table>
+        <tr>
+            <td><b>Name:</b></td>
+            <td><input name="accountName" type="text"><br></td>
+        </tr>
+        <tr>
+            <td><b>Phone:</b></td>
+            <td><input name="accountPhone" type="text"><br></td>
+        </tr>
+        <tr>
+            <td><b>Address:</b></td>
+            <td><input name="accountAddress" type="text"><br></td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center"><input type="submit" value="Create"><br></td>
+        </tr>
+    </table>
 </form>
+    </center>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,14 +3,39 @@
         <title>The Phonebook Project</title>    
     </head>
     <body>
-        <nav>
-            <ul>
-                <li><u>Contacts</u></li>
-                <li><a href="createNew.php">Create</a></li>
-                <li><a href="editList.php">Edit list</a></li>
-                <li><a href="search.php">Search</a></li>
-            </ul>
-        </nav>        
+        <!--MENU-->
+        <table>
+            <tr>
+                <td>The Phonebook Project | </td>
+                <td><a href="createNew.php">New | </a></td>
+                <td><a href="editList.php">Edit | </a></td>
+                <td><a href="search.php">Search | </a></td>
+            </tr>
+        </table>         
+        
+        <!--DBLIST-->
+        <center>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Address</th>
+            </tr>
+            <?php
+                $connection = mysqli_connect("localhost", "root", "root", "contactlist");
+                $sql = "SELECT * FROM contactlist";
+                $result = $connection->query($sql);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr><td>" . $row["accountName"] . "</td><td>" . $row["accountPhone"] . "</td><td>" . $row["accountAddress"] . "</td></tr>";
+                    }
+                } else {
+                    echo "No contacts in phonebook<br>";
+                }
+            ?>
+        </table>
+        </center>
+
         <footer><center>The Phonebook Project | <?php include "version.php"?></center></footer>
     </body>
 </html>
