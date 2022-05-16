0092306914bd93b8d10e380694066314c3dee865
Gennadii <35961691+brundrom@users.noreply.github.com>
2022-05-16 22:08:54 +0300

    Add files via upload
    Edited file for work with db

---
+++ dbWorker.php
@@ -0,0 +1,12 @@
+<?php
+    class dbWorker() {
+        $username = "root";
+        $password = "root";
+        $dbName = "contactlist";
+        $serverName = "localhost"
+
+        function connectToDB($serverName, $username, $password, $dbName){
+            echo "Connection to database.\n";
+        }
+    }
+?>
